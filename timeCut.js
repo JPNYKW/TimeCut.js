46b6d6638efb2a246773a5b1bd82a06c1c27a5c9
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2017-11-20 00:59:07 +0900

    Add files via upload

---
+++ timeCut.js
@@ -0,0 +1,23 @@
+var abs=(m=Math).abs;
+var atan2=m.atan2;
+var round=m.round;
+var floor=m.floor;
+var ceil=m.ceil;
+var sqrt=m.sqrt;
+var asin=m.asin;
+var acos=m.acos;
+var atan=m.atan;
+var pow=m.pow;
+var sin=m.sin;
+var cos=m.cos;
+var tan=m.tan;
+
+var d=document;
+var c=console;
+var w=window;
+
+var now=()=>{return new Date()};
+Date.prototype.ms=function(){return this.getTime()}
+Date.prototype.sec=function(){return this.getSeconds()}
+Date.prototype.min=function(){return this.getMinutes()}
+Date.prototype.hours=function(){return this.getHours()}
