acc9d2c878ca6dd3d4e7aa9172bd74f76a0780d6
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2017-11-27 19:34:56 +0900

    Update timeCut.js

--- timeCut.js
+++ timeCut.js
@@ -1,3 +1,9 @@
+// Classes
+var d=document;
+var c=console;
+var w=window;
+
+// Math
 var abs=(m=Math).abs;
 var atan2=m.atan2;
 var round=m.round;
@@ -12,13 +18,18 @@ var sin=m.sin;
 var cos=m.cos;
 var tan=m.tan;
 
-var d=document;
-var c=console;
-var w=window;
+// Sum
+var direction=(x,y,x1,y1)=>{return atan2(x-x1,y-y1)};
+var distance=(x,y,x1,y1)=>{return sqrt(pow(x-x1,2)+pow(y-y1,2))};
 
+// Date
 var now=()=>{return new Date()};
 Date.prototype.ms=function(){return this.getTime()}
 Date.prototype.sec=function(){return this.getSeconds()}
 Date.prototype.min=function(){return this.getMinutes()}
 Date.prototype.hours=function(){return this.getHours()}
+
+// Array
 Array.prototype.last=function(){return this[this.length-1]}
+Array.prototype.average=function(){sum=0;this.map(e=>sum+=e);return sum/this.length}
+
